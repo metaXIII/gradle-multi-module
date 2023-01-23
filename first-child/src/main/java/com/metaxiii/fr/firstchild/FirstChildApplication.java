61a3a091aed593c519c6dec56a03ee8666b327e7
metaxiii <lehchibi.gael@pm.me>
2023-01-23 08:56:06 +0100

    feat: add first child project

---
+++ first-child/src/main/java/com/metaxiii/fr/firstchild/FirstChildApplication.java
@@ -0,0 +1,13 @@
+package com.metaxiii.fr.firstchild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstChildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstChildApplication.class, args);
+    }
+
+}
---
+++ first-child/src/test/java/com/metaxiii/fr/firstchild/FirstChildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaxiii.fr.firstchild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstChildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
